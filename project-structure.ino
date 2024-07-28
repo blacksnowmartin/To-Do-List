28555c8ff2a6c3f315a7e354402bb3266314a9cc
Martin Kitonga <119602263+blacksnowmartin@users.noreply.github.com>
2024-07-29 00:04:29 +0300

    Create project-structure.ino

---
+++ project-structure.ino
@@ -0,0 +1,14 @@
+to_do_list_app/
+├── app/
+│   ├── static/
+│   │   ├── styles.css
+│   │   └── scripts.js
+│   ├── templates/
+│   │   └── index.html
+│   ├── __init__.py
+│   ├── models.py
+│   └── routes.py
+├── venv/
+├── .gitignore
+├── requirements.txt
+└── run.py
